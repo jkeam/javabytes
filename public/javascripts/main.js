815cc72960121e55381c34e66e8cc451278adb6f
Jon Keam <jpkeam@gmail.com>
2012-06-08 19:55:54 -0400

    added ability to send to server and receive response

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,31 @@
-function submitCode() {
-            var jsonText = JSON.stringify({"code":"System.out.println(\"hi\");"});
-            $.ajax({
-                type: 'POST',
-                url: '/',
-                dataType: 'json',
-                contentType: 'application/json; charset=utf-8',
-                data: jsonText,
-                success: function(json) {
-                  console.log(json.code)
-                  alert(json.code)
-                },
-                failure: function(xhr, textStatus, errorThrown) {
-                  window.alert('failure: ');
-                }
-            });
-          }
+function Javabytes() {
+  var inputMirror;
+  var outputMirror;
 
-function onReady() {
-    var inputMirror = CodeMirror.fromTextArea($("#inputTextArea").get(0), {lineNumbers:true});
-    var outputMirror = CodeMirror.fromTextArea($("#outputTextArea").get(0), {lineNumbers:true, readOnly :true});
+  this.submitCode= function() {
+    var rawText = inputMirror.getValue();
+    if (!rawText) return;
+    var jsonText = JSON.stringify({"code":rawText});
+    $.ajax({
+      type: 'POST',
+      url: '/',
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+      data: jsonText,
+      success: function(json) {
+        var code = json.code;
+        code = code.replace(/<br>/g, "\n");
+        outputMirror.setValue(code);
+      },
+      failure: function(xhr, textStatus, errorThrown) {
+        window.alert('failure: ');
+      }
+    });
+  };
+ 
+  this.onReady= function() {
+    inputMirror = CodeMirror.fromTextArea($("#inputTextArea").get(0), {lineNumbers:true});
+    outputMirror = CodeMirror.fromTextArea($("#outputTextArea").get(0), {lineNumbers:true, readOnly :true});
+  };
 }
-
+var javabytes = new Javabytes();
