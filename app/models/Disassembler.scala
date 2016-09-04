cb79ef9160555626d983f37767318e0d7fa4d504
Jonathan Keam <jpkeam@gmail.com>
2016-09-03 22:51:53 -0400

    added period to tmp dir

--- app/models/Disassembler.scala
+++ app/models/Disassembler.scala
@@ -7,7 +7,7 @@ import scala.sys.process._
 import scalax.io._
 
 class Disassembler {
-  val tmpDir = "data/"
+  val tmpDir = "./data/"
 
   def disassemble(code:String):String = {
     val logger = new Logger()
